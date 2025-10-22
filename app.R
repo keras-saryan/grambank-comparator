908fd2365fc06935ee4b35274d1b876d6252443c
Keras Saryan <80210662+keras-saryan@users.noreply.github.com>
2025-10-22 13:37:32 +0100

    Added (sub)category and parameter filtering

--- app.R
+++ app.R
@@ -25,11 +25,19 @@ grambank_wide <- open_dataset("grambank_wide") %>%
   select(-Language_Level) %>%
   relocate(Language_ID, Language_Name, Language_Macroarea, Language_Family_Name, Parameters_Coded)
 
+grambank_parameters <- as.character(colnames(grambank_wide %>% select(GB020:GB522)))
+
+grambank_parameter_categories <- read_tsv("grambank_parameter_categories.tsv")
+
+grambank_categories <- sort(unique(grambank_parameter_categories$Parameter_Category))
+
+grambank_subcategories <- sort(unique(grambank_parameter_categories$Parameter_Subcategory))
+
 fontCustom <- "Noto Sans"
 
 dark_grey <- "#bfbfbf"
 light_grey <- "#dfdfdf"
-lighter_grey <- "#e3e3e3"
+lighter_grey <- "#ededed"
 mid_green <- "#00aa00"
 
 theme_set(
@@ -45,7 +53,7 @@ theme_set(
     )
 )
 
-similarity_colours <- c("#ffffff", "#ff0000", "#0000ff", "#000088", "#000000")
+similarity_colours <- c("#ffaaaa", "#ff0000", "#0000ff", "#000088", "#000022")
 similarity_colours_8 <- colorRampPalette(similarity_colours)(8)
 similarity_colours_10 <- colorRampPalette(similarity_colours)(10)
 similarity_colours_100 <- colorRampPalette(similarity_colours)(100)
@@ -92,7 +100,9 @@ ui <- page_fillable(
       
       width = 350,
       
-      markdown("Use the field below to upload a tab-separated file detailing the Grambank features of your language. Click the \"Compare\" button to see which languages from Grambank's database are most similar—this will take a few seconds to load! The button below this also provides a downloadable TSV of the comparison table."),
+      markdown("Use the field below to upload a tab-separated file detailing the Grambank features of your language. Click the \"Compare\" button to see which languages from Grambank's database are most similar—this will take a few seconds to load!"),
+      
+      markdown("For more details on the app and type of file required, see the \"About\" tab."),
       
       fileInput("file", "", accept = c(".tsv", ".csv", "text/tab-separated-values")),
       
@@ -107,7 +117,32 @@ ui <- page_fillable(
                    step = 1,
                    value = 50),
       
-      markdown("For more details on the app and type of file required, see the \"About\" tab."),
+      selectInput(
+        inputId = "categories_to_compare",
+        label = "Categories To Compare:",
+        choices = grambank_categories,
+        multiple = TRUE,
+        selected = grambank_categories
+      ),
+      
+      actionButton("reset_categories", "Reset Categories", class = "btn-secondary"),
+      
+      selectInput(
+        inputId = "subcategories_to_compare",
+        label = "Subcategories To Compare:",
+        choices = grambank_subcategories,
+        multiple = TRUE,
+        selected = grambank_subcategories
+      ),
+      
+      actionButton("reset_subcategories", "Reset Subcategories", class = "btn-secondary"),
+      
+      selectInput(
+        inputId = "parameter_to_compare",
+        label = "Select A Single Parameter:",
+        choices = c("Compare All", grambank_parameters),
+        selected = "Compare All"
+      )
       
     ),
     
@@ -199,7 +234,15 @@ ui <- page_fillable(
           
           The input field in the sidebar expects a tab-separated file (e.g. `.tsv`, `.csv`, `.txt`) based on [Jessie Peterson](https://www.quothalinguist.com/)'s \"[Grambank Features List for Language Documentation](https://docs.google.com/spreadsheets/d/18Wdhtx7w5SHbe3GmkhFgTE7fjBiEX7SaL4O2-dFibB0/edit?gid=0#gid=0)\" Google Sheet ([see also her explanatory *Fiat Lingua* article](https://fiatlingua.org/2023/06/)). The easiest way to get this is to export your version of the spreadsheet as a TSV. It doesn't matter if the column names have been changed but the content of the original columns should remain in those same columns (and on the same, single sheet). It is also important that the \"Y/N\" column use the same set of values as the original spreadsheet; the app will treat any other values (or any absent values) as equivalent to `NA`.
           
-          Clicking \"Compare\" will produce a table in the \"Table\" tab which has eight columns:
+          The app simply compares the (non-NA-equivalent) feature values in the user-supplied file with the corresponding values for each language in Grambank's sample to see what proportion of these match on a per-language basis; all features are thus weighted equally when considering \"similarity\". The data used include only individual *languages* in Grambank and not those entries labelled as *dialects* or *families*.
+          
+          Clicking \"Compare\" will produce outputs in the \"Table\", \"Plot\" and \"Map\" tabs.
+          
+          A numeric input box in the sidebar can be used to change the minimum number of comparisons between the user-supplied language and the Grambank sample languages required to be displayed in the table, plots and map.
+          
+          You can filter the data set used in the comparison by the category and/or subcategory of parameters as coded in Jessie's spreadsheet using the \"Categories To Compare\" and \"Subcategories To Compare\" select boxes. You can also choose to view an individual parameter. Note that (a.) this might take a few seconds to update (potentially a lot longer if you individually delete a lot of subcategories at once) and (b.) it will probably necessary to lower the \"Total Compared\" minimum.
+          
+          The \"Table\" tab shows a table with the following eight columns:
           
           1. **ID:** The ID of that row's language as found in Grambank.
           2. **Language:** The name of that row's language.
@@ -210,17 +253,13 @@ ui <- page_fillable(
           7. **Match Count:** The number of compared features that are the same in that row's language and the novel language.
           8. **Similarity (%):** The ratio of Total Compared to Match Count expressed as a percentage.
           
-          The app simply compares the (non-NA-equivalent) feature values in the user-supplied file with the corresponding values for each language in Grambank's sample to see what proportion of these match on a per-language basis; all features are thus weighted equally when considering \"similarity\" (perhaps one reason you might get some unexpected results). Note that the data used include only individual *languages* in Grambank and not those entries labelled as *dialects* or *families*.
+          The table is equipped with a general search bar but each column can also be individually searched or the numeric columns' ranges changed to facilitate filtering. A TSV version of this table can be downloaded using the button in the sidebar.
           
-          The table is equipped with a general search bar but each column can also be individually searched or the numeric columns' ranges changed to facilitate filtering. A TSV version of this table can be downloaded in the sidebar.
-          
-          The \"Plot\" tab shows the same data as in \"Table\" but plotted as a histogram, with the options of facetting by macroarea, continent, UN subregion ([sourced here](https://github.com/lukert33/united-nations-geoscheme-subregions-json)) and language family—the final two can be further tweaked by changing the required minimum number of languages in a subregion/family for plotting.
+          The \"Plot\" tab shows the same data as in \"Table\" but plotted as a histogram, with the options of facetting by macroarea, continent, UN subregion ([sourced here](https://github.com/lukert33/united-nations-geoscheme-subregions-json)) and language family—the final two can be further tweaked by changing the required minimum number of languages in a subregion/family for plotting. Plots can be saved by right clicking and choosing the appropriate option from the context menu.
           
           The \"Map\" tab shows a world map with circles for each language in Grambank colour coded according to their similarity to the user-supplied language data. Clicking the circle will show the language's name and the percentage similarity.
           
-          In the histograms and map, the <span style='color:#f00000; font-weight:bold;'>redder</span> a column/circle, the more similar a language; the <span style='color:#0000f0; font-weight:bold;'>bluer</span> a column/circle, the more different the language; however, extreme similarities/differences are represented by white/black respectively since these will usually be extreme outliers.
-          
-          The numeric input box in the sidebar can be used to change the minimum number of comparisons between the user-supplied language and the Grambank sample languages required to be displayed in the table, plots and map.
+          In the histograms and map, the <span style='color:#f00000; font-weight:bold;'>redder</span> a bar/circle, the more similar a language; the <span style='color:#0000f0; font-weight:bold;'>bluer</span> a bar/circle, the more different the language; however, extreme similarities/differences approach white/black respectively since these will usually be extreme outliers unless you are radically paring the data down.
           
           Consult Grambank itself for more on individual [features](https://grambank.clld.org/parameters) and [languages](https://grambank.clld.org/languages).
           
@@ -239,49 +278,100 @@ ui <- page_fillable(
 
 server <- function(input, output, session) {
   
-  results <- eventReactive(input$run, {
+  observeEvent(input$reset_categories, {
+    updateSelectInput(
+      session,
+      "categories_to_compare",
+      selected = grambank_categories
+    )
+  })
+  
+  observeEvent(input$reset_subcategories, {
+    updateSelectInput(
+      session,
+      "subcategories_to_compare",
+      selected = grambank_subcategories
+    )
+  })
+  
+  grambank_filtered <- reactive({
+    
+    req(input$categories_to_compare)
+    req(input$subcategories_to_compare)
+    req(input$parameter_to_compare)
+    
+    if (input$parameter_to_compare != "Compare All") {
+      
+      excluded_parameters <- grambank_parameter_categories %>%
+        filter(Parameter_ID != input$parameter_to_compare) %>%
+        pull(Parameter_ID)
+      
+      grambank_wide %<>%
+        mutate(across(all_of(excluded_parameters), ~ NA))
+      
+    } else if (input$parameter_to_compare == "Compare All") {
+      
+      excluded_parameters <- grambank_parameter_categories %>%
+        filter(!(Parameter_Category %in% input$categories_to_compare) | !(Parameter_Subcategory %in% input$subcategories_to_compare)) %>%
+        pull(Parameter_ID)
+      
+      grambank_wide %<>%
+        mutate(across(all_of(excluded_parameters), ~ NA))
+      
+    }
+    
+  })
+  
+  input_lang <- eventReactive(input$run, {
     
     req(input$file)
     
-    withProgress(message = "Reading input and comparing with Grambank...", value = NULL, {
+    input_lang <- read.csv(input$file$datapath, header = TRUE, sep = "\t", quote = "", stringsAsFactors = FALSE, encoding = "UTF-8") %>%
+      select(-2) %>%
+      rename(Parameter_ID = 1, Parameter_Category = 2, Parameter_Subcategory = 3, Parameter_Value_Long = 4, Parameter_Comment = 5) %>%
+      mutate(Parameter_Value_Long = str_trim(Parameter_Value_Long),
+             Parameter_Value_Long = case_when(
+               Parameter_Value_Long == "Yes" ~ "present",
+               Parameter_Value_Long == "No" ~ "absent",
+               Parameter_Value_Long == "Both" ~ "both",
+               Parameter_Value_Long == "Not sure" ~ NA_character_,
+               TRUE ~ Parameter_Value_Long)) %>%
+      unique() %>%
+      mutate(Language_ID = "input_lang", Language_Name = "input_lang", Language_Macroarea = "input_lang", Language_Family_Name = "input_lang") %>%
+      relocate(Language_ID, Language_Name, Language_Macroarea, Language_Family_Name)
+    
+    input_lang %>%
+      select(Language_ID, Language_Name, Language_Macroarea, Language_Family_Name, Parameter_ID, Parameter_Value_Long) %>%
+      pivot_wider(names_from = Parameter_ID, values_from = Parameter_Value_Long) %>%
+      rowwise() %>%
+      mutate(Parameters_Coded = sum(!is.na(c_across(GB020:GB522)))) %>%
+      ungroup() %>%
+      relocate(Language_ID, Language_Name, Language_Macroarea, Language_Family_Name, Parameters_Coded)
+    
+  })
+  
+  results <- reactive({
+    
+    req(input$file)
+    
+    withProgress(message = "Comparing input with Grambank...", value = NULL, {
       
-      input_lang <- read.csv(input$file$datapath, header = TRUE, sep = "\t", quote = "", stringsAsFactors = FALSE, encoding = "UTF-8") %>%
-        select(-2) %>%
-        rename(Parameter_ID = 1, Parameter_Category = 2, Parameter_Subcategory = 3, Parameter_Value_Long = 4, Parameter_Comment = 5) %>%
-        mutate(Parameter_Value_Long = str_trim(Parameter_Value_Long),
-               Parameter_Value_Long = case_when(
-                 Parameter_Value_Long == "Yes" ~ "present",
-                 Parameter_Value_Long == "No" ~ "absent",
-                 Parameter_Value_Long == "Both" ~ "both",
-                 Parameter_Value_Long == "Not sure" ~ NA_character_,
-                 TRUE ~ Parameter_Value_Long)) %>%
-        unique() %>%
-        mutate(Language_ID = "input_lang", Language_Name = "input_lang", Language_Macroarea = "input_lang", Language_Family_Name = "input_lang") %>%
-        relocate(Language_ID, Language_Name, Language_Macroarea, Language_Family_Name)
+      non_na_positions <- which(!is.na(input_lang() %>% select(GB020:GB522)))
       
-      input_lang_wide <- input_lang %>%
-        select(Language_ID, Language_Name, Language_Macroarea, Language_Family_Name, Parameter_ID, Parameter_Value_Long) %>%
-        pivot_wider(names_from = Parameter_ID, values_from = Parameter_Value_Long) %>%
-        rowwise() %>%
-        mutate(Parameters_Coded = sum(!is.na(c_across(GB020:GB522)))) %>%
-        ungroup() %>%
-        relocate(Language_ID, Language_Name, Language_Macroarea, Language_Family_Name, Parameters_Coded)
+      non_na_values <- input_lang() %>% select(GB020:GB522)
       
-      sum(!is.na(input_lang_wide %>% select(GB020:GB522)))
-      
-      non_na_positions <- which(!is.na(input_lang_wide %>% select(GB020:GB522)))
-      
-      similarity <- grambank_wide %>%
+      similarity <- grambank_filtered() %>%
         rowwise() %>%
         mutate(
           Row_Values = list(c_across(GB020:GB522)),
-          Match_Count = sum(unlist(Row_Values) == input_lang_wide %>% select(GB020:GB522), na.rm = TRUE),
-          Total_Compared = sum(!is.na(unlist(Row_Values)) & !is.na(input_lang_wide %>% select(GB020:GB522))),
+          Parameters_Coded = sum(!is.na(unlist(Row_Values))),
+          Match_Count = sum(unlist(Row_Values) == non_na_values, na.rm = TRUE),
+          Total_Compared = sum(!is.na(unlist(Row_Values)) & !is.na(non_na_values)),
           Percentage_Similarity = 100 * Match_Count / Total_Compared
         ) %>%
         ungroup() %>%
         select(Language_ID, Language_Name, Language_Family_Name, Language_Macroarea, Parameters_Coded, Total_Compared, Match_Count, Percentage_Similarity) %>%
-        mutate(Percentage_Similarity = as.numeric(format(round(Percentage_Similarity, 2), nsmall = 2))) %>%
+        mutate(Percentage_Similarity = round(Percentage_Similarity, 2)) %>%
         arrange(desc(Percentage_Similarity)) %>%
         rename(ID = Language_ID, Language = Language_Name, Family = Language_Family_Name, Macroarea = Language_Macroarea, `Similarity (%)` = Percentage_Similarity)
       
@@ -295,6 +385,11 @@ server <- function(input, output, session) {
     req(results())
     results() %>%
       filter(`Total Compared` >= input$compared_min)
+  })
+  
+  spatial_results <- reactive({
+    req(final_results())
+    full_join(final_results(), grambank_coordinates, by = "ID")
   })
   
   output$results <- renderDataTable(server = TRUE, {
@@ -344,14 +439,14 @@ server <- function(input, output, session) {
       
     } else if (input$facet_toggle == "continent") {
       
-      continent_levels <- full_join(final_results(), grambank_coordinates, by = "ID") %>%
+      continent_levels <- spatial_results() %>%
         filter(!is.na(Continent)) %>%
         group_by(Continent) %>%
         summarise(Mean_Similarity = mean(`Similarity (%)`, na.rm = TRUE)) %>%
         arrange(desc(Mean_Similarity)) %>%
         pull(Continent)
       
-      full_join(final_results(), grambank_coordinates, by = "ID") %>%
+      spatial_results() %>%
         filter(!is.na(Continent)) %>%
         group_by(Continent) %>%
         mutate(Mean_Similarity = mean(`Similarity (%)`, na.rm = TRUE), Continent = factor(Continent, levels = continent_levels)) %>%
@@ -365,7 +460,7 @@ server <- function(input, output, session) {
       
     } else if (input$facet_toggle == "subregion") {
       
-      subregion_levels <- full_join(final_results(), grambank_coordinates, by = "ID") %>%
+      subregion_levels <- spatial_results() %>%
         filter(!is.na(Subregion)) %>%
         group_by(Subregion) %>%
         summarise(Mean_Similarity = mean(`Similarity (%)`, na.rm = TRUE), n_languages = n_distinct(ID)) %>%
@@ -374,7 +469,7 @@ server <- function(input, output, session) {
         slice_head(n = 6) %>%
         pull(Subregion)
       
-      full_join(final_results(), grambank_coordinates, by = "ID") %>%
+      spatial_results() %>%
         filter(Subregion %in% subregion_levels) %>%
         group_by(Subregion) %>%
         mutate(Mean_Similarity = mean(`Similarity (%)`, na.rm = TRUE), Subregion = factor(Subregion, levels = subregion_levels)) %>%
@@ -450,7 +545,8 @@ server <- function(input, output, session) {
   
   output$comparison_map <- renderLeaflet({
     
-    mapping <- full_join(final_results(), grambank_coordinates, by = "ID") %>%
+    mapping <- spatial_results() %>%
+      drop_na(`Similarity (%)`) %>%
       filter(`Similarity (%)` >= 0 & `Similarity (%)` <= 100) %>%
       mutate(Colour = case_when(
         `Similarity (%)` >= 87.5 & `Similarity (%)` <= 100 ~ similarity_colours_8[1],
